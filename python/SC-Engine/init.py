4e5006fcf92b5bd381ae7db57a58f2921b351d7e
prem0132 <prem.it.rits@gmail.com>
2018-06-07 20:53:24 +0530

    just some cosmeticchanges

--- python/SC-Engine/init.py
+++ python/SC-Engine/init.py
@@ -3,17 +3,20 @@ import requests
 import socket
 
 ###############CHAIN OF EVENTS###############
-def chainofevents():
-    print("Welcome to the future\n")
+def samplegetcall():
+    print("This is a Sample get call using hardcoded authentication \n")
     URL = "http://127.0.0.1:5000/redfish/v1/AccountService/"
     r = requests.get(url = URL, auth=('root', 'password123456'))
+    print("URL: %s \n"%URL)
     returncode = r.status_code
     print("Headers: %s\n"%(r.headers))
     if(returncode == 200):
         print("GET Call Successful\n")
+        print("Status Code: %s\n"%(returncode)) 
         data = r.json()
-        print("data: %s\n"%(data))
+        print("Data:\n %s\n"%(data))
     else:
+        print("GET Call Failed\n")
         print("Status Code: %s\n"%(returncode)) 
     return returncode
 
@@ -21,10 +24,12 @@ def chainofevents():
 
 ################main function###############
 def main():
-	print("Initiating test......\n")
-	chainofevents()
+	print("Initiating Smart Component Installation......\n")
+	samplegetcall()
 	print("The test has completed. Generating log file....")
 	print('the test has successfully completed. Please check logs.')
+
+
 
 #############calling main function############
 if __name__ == "__main__":
